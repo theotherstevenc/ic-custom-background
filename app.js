2f1f3665624cb477a32a53ddfba6bad927f0ec75
stevencollins <s.collins@salesforce.com>
2022-12-15 11:38:52 -0800

    consolidate api into current app

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+require('dotenv').config()
+
 const express = require('express')
 const _ = require('lodash')
+const fetch = require('node-fetch')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+
 app.use('/', express.static('dist'))
+
+app.get('/font-api', (req,res)=>{
+
+  const limit = req.query.limit || 5
+  const sortby = req.query.sort || 'popularity'
+
+  fetch(`${process.env.API_BASE_URL}?key=${process.env.API_KEY}&sort=${sortby}`)
+    .then(res => res.json())
+    .then(data => {
+      res.send(JSON.stringify(
+        data.items.map(el => el.family)
+        .slice(0,limit)
+      ))
+    });
+  
+})
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`localhost//:3000`)
 })
